450f24eb11bb8019d927ad2ca8906834c1267db2
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2016-04-30 11:40:55 +0200

    Initial Commit

---
+++ BrewServicesMenubar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BrewServicesMenubar
+//
+//  Created by Andrew on 30/04/2016.
+//  Copyright © 2016 andrewnicolaou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
