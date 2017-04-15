292d4957366238785116593506309a1155763020
Stéphan Kochen <stephan@kochen.nl>
2017-04-15 10:10:02 +0200

    Remove unused matchesFoRegexInText

--- BrewServicesMenubar/AppDelegate.swift
+++ BrewServicesMenubar/AppDelegate.swift
@@ -14,20 +14,6 @@ struct Service {
     var user = ""
 }
 
-func matchesForRegexInText(_ regex: String!, text: String!) -> [String] {
-    do {
-        let regex = try NSRegularExpression(pattern: regex, options: [])
-        let nsString = text as NSString
-        let results = regex.matches(in: text,
-                                            options: [], range: NSMakeRange(0, nsString.length))
-        return results.map { nsString.substring(with: $0.range)}
-    } catch let error as NSError {
-        print("invalid regex: \(error.localizedDescription)")
-        return []
-    }
-}
-
-
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
